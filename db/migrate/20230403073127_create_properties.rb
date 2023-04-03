486eb92ce9ab444049f8a0f0eda08357a32c84aa
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-03 10:45:38 +0300

    Add a Partial Link[Navigations]

--- db/migrate/20230403073127_create_properties.rb
+++ db/migrate/20230403073127_create_properties.rb
@@ -1,11 +1,14 @@
 class CreateProperties < ActiveRecord::Migration[7.0]
   def change
     create_table :properties do |t|
+      # connect our account
+      t.references :account
       t.string :name
       t.string :address
       t.integer :price
       t.integer :rooms
       t.integer :bathrooms
+      t.string :photo
 
       t.timestamps
     end
